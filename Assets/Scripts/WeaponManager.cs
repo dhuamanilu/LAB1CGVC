b6a3256c2429c0d653ec52dc5ceafcdf3261e570
Diego Alonso Huamani Luque <dhuamanilu@unsa.edu.pe>
2025-05-05 19:54:51 -0500

    implementando logica de disparo, solo se avanzo hasta detectarlo
    Former-commit-id: ba23e62c5ccb188eefb7f20357ebecbc9aaa0be3

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public GameObject playerCam;
+    public float range = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Debug.Log("shoot!");
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(playerCam.transform.position, transform.forward, out hit, range))
+        {
+            Debug.Log("hit");
+        }
+    }
+}
