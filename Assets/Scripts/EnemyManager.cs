2a33af68299f347c77b0d433b78d0a4232cb8cc1
Diego Alonso Huamani Luque <dhuamanilu@unsa.edu.pe>
2025-05-05 19:14:05 -0500

    integrando logica de animacion para que el zombie este animado cuando corra
    Former-commit-id: f42b4ff751a201eb9ba34baee622aef3b8b94060

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class EnemyManager : MonoBehaviour
 {
     public GameObject player;
+    public Animator enemyAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,14 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<NavMeshAgent>().destination=player.transform.position;
+        if(GetComponent<NavMeshAgent>().velocity.magnitude > 1)
+        {
+            enemyAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            enemyAnimator.SetBool("isRunning", false);
+        }
     }
 }
