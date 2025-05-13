6b5ffda68b700074a50a2867585b8c29443bdb0a
Diego Alonso Huamani Luque <dhuamanilu@unsa.edu.pe>
2025-05-12 23:09:23 -0500

    Realizando mejoras , ahora las balas son objetos con fisicas de unity

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
-    public GameObject playerCam;
-    public float range = 100f;
-    public float damage = 25f;
+    public Transform firePoint;          // vacío en la punta del arma
+    public GameObject bulletPrefab;      // arrastra aquí tu Bullet
     public Animator playerAnimator;
     // Start is called before the first frame update
     void Start()
@@ -17,28 +16,12 @@ public class WeaponManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerAnimator.GetBool("isShooting"))
-        {
-            playerAnimator.SetBool("isShooting", false);
-        }
         if (Input.GetButtonDown("Fire1"))
-        {
-            //Debug.Log("shoot!");
             Shoot();
-
-        }
     }
     void Shoot()
     {
-        playerAnimator.SetBool("isShooting", true);
-        RaycastHit hit;
-        if(Physics.Raycast(playerCam.transform.position, transform.forward, out hit, range))
-        {
-            Debug.Log("hit");
-            EnemyManager enemyManager = hit.transform.GetComponent<EnemyManager>();
-            if (enemyManager != null) {
-                enemyManager.Hit(damage);
-            }
-        }
+        //playerAnimator.SetTrigger("Shoot");
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
